d4ec7a3e2a86166ba7fd365b6ff7f86f3e292aa6
RSwetha14 <swethar1414@gmail.com>
2023-03-16 20:03:41 +0530

    feat-schemes ft.swe

--- app.py
+++ app.py
@@ -4,17 +4,17 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://sukesh:sukesh@2002@localhost/mydatabase'
 
+@app.route("/<name>")
+def schemes(name):
+     return render_template("schemes.html", name=name)
 
 @app.route("/<name>")
 def fund(name):
      return render_template("fund.html", name=name)
+
 @app.route("/<name>")
 def welcome(name):
 	return render_template("about.html", name=name)
-
-@app.route("/<name>")
-def schemes(name):
-    return render_template("schemes.html", name=name)
 
 @app.route('/')
 def index():
