0a91b311f052ec7b71e565381e4fd2b875d6510c
tejaswini3012 <tejaswini3012@gmail.com>
2023-03-17 22:04:31 +0530

    feat-schemes(teja)

--- app.py
+++ app.py
@@ -4,22 +4,21 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://sukesh:sukesh@2002@localhost/mydatabase'
 
+@app.route("/sch")
+def sch():
+     return render_template("sch.html")
+
+@app.route("/fund")
+def fund():
+     return render_template("fund.html")
+
+@app.route("/about")
+def welcome():
+	return render_template("about.html")
 
 @app.route("/<name>")
 def schemes(name):
-     return render_template("schemes.html", name=name)
-  
-@app.route('/<name>')
-def bod(name):
-    return render_template('bod.html',name=name)
-
-@app.route('/<name>')
-def fund(name):
-     return render_template("fund.html", name=name)
-
-@app.route('/<name>')
-def about(name):
-    return render_template('about.html', name=name)
+    return render_template("schemes.html", name=name)
 
 @app.route('/')
 def index():
