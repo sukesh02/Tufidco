4f26a82c57b24d0099ed8a266ad1692ed66aaefd
sukesh02 <sukeshraghav@gmail.com>
2023-03-17 22:14:53 +0530

    fix-routechanges

--- app.py
+++ app.py
@@ -1,29 +1,44 @@
-from flask import Flask,render_template,url_for
+from flask import Flask,render_template,url_for,request
+import psycopg2
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://sukesh:sukesh@2002@localhost/mydatabase'
-
-
-@app.route("/<name>")
-def schemes(name):
-     return render_template("schemes.html", name=name)
-  
-@app.route('/<name>')
-def bod(name):
-    return render_template('bod.html',name=name)
-
-@app.route('/<name>')
-def fund(name):
-     return render_template("fund.html", name=name)
-
-@app.route('/<name>')
-def about(name):
-    return render_template('about.html', name=name)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route("/schemes")
+def schemes():
+    return render_template("schemes.html")
+
+@app.route('/bod')
+def bod():
+    return render_template('bod.html')
+
+@app.route('/fund')
+def fund():
+    return render_template("fund.html")
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+conn = psycopg2.connect(database="schemes", 
+                        user="sukku",
+                        password="schemes", 
+                        host="localhost", port="5432")
+
+cur = conn.cursor()
+
+cur.execute('''SELECT * FROM schemes_data''')
+
+data = cur.fetchall()
+
+conn.commit()
+
+cur.close()
+conn.close()
